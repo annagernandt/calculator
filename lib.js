2717b8b6e001681c09db0347da627ce8d2c1ca0d
GEA (Anna Gernandt) <gea@cim.local>
2018-02-16 15:51:00 +0100

    zweiter commit

---
+++ lib.js
@@ -0,0 +1,43 @@
+function clickbutton0(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "0";	
+}
+
+function clickbutton1(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "1";	
+}
+
+function clickbutton2(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "2";	
+}
+
+function clickbutton3(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "3";	
+}
+
+function clickbutton4(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "4";	
+}
+
+function clickbutton5(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "5";	
+}
+
+function clickbutton6(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "6";	
+}
+
+function clickbutton7(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "7";	
+}
+
+function clickbutton8(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "8";	
+}
+
+function clickbutton9(){ 
+	document.getElementById("task").value = document.getElementById("task").value + "9";	
+}
+
+function clickbuttonc(){
+	document.getElementById("task").value='';
+}
